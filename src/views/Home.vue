6c2fc2793041339117c4822bc926e821a3dbdca6
Bogdan <4imzuk@gmail.com>
2025-03-11 21:04:57 +0100

    - bseic homepage with Image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
-    <iframe
-      src="/main/index.html"
-      frameborder="0"
-      allowfullscreen
-      class="iframe-demo"
-    ></iframe>
+
   </div>
 </template>
 
@@ -15,19 +10,49 @@ export default {
 }
 </script>
 
+<style>
+body {
+  background-color: transparent;
+}
+</style>
+
 <style scoped>
 .home {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
   height: 100vh;
   width: 100vw;
   margin: 0;
   padding: 0;
+  background-image: url('https://placehold.co/1920x1080');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.iframe-demo {
+.content {
+  text-align: center;
+  color: white;
+  padding: 2rem;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 8px;
+}
+
+h1 {
+  font-size: 3rem;
+  margin: 0;
+}
+
+/* .iframe-demo {
   height: 100%;
   width: 100%;
   border: none;
-}
+} */
 
 /* h1 {
     position: absolute;
