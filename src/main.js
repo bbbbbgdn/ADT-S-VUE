ab1307010620aa16330d2868203c73b3d33febe9
Bogdan <4imzuk@gmail.com>
2025-03-12 02:08:00 +0100

    - updated Storyblock Integration - moved acess keys

--- src/main.js
+++ src/main.js
@@ -4,19 +4,20 @@ import router from './router';
 import { StoryblokVue, apiPlugin } from '@storyblok/vue';
 import DynamicComponent from './components/DynamicComponent.vue';
 
-// Створи додаток
 const app = createApp(App);
 
-// Підключи Storyblok
 app.use(StoryblokVue, {
-  accessToken: 'jVwJlAUbh5ZbmDF1SC5OHQtt', // Замініть на свій API токен
+  accessToken: import.meta.env.VITE_STORYBLOK_PREVIEW_TOKEN,
+  bridge: import.meta.env.NODE_ENV !== 'production', // optimizes by excluding the bridge on production
   use: [apiPlugin],
+
+  apiOptions: {
+    region: "eu",
+ },
 });
 
 app.component('StoryblokComponent', DynamicComponent);
 
-// Підключи маршрутизацію
 app.use(router);
 
-// Монтируй додаток
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,17 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import fs from 'fs';
 import path from 'path';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    basicSsl(),
+  ],
   server: {
-    // https: {
-    //   // You can use self-signed certificates for local development
-    //   // If you don't have certificates, Vite will generate them for you
-    //   // But for better compatibility, you can create your own
-    //   key: fs.existsSync('localhost-key.pem') ? fs.readFileSync('localhost-key.pem') : undefined,
-    //   cert: fs.existsSync('localhost.pem') ? fs.readFileSync('localhost.pem') : undefined,
-    // },
-    host: true, // Needed for accessing from other devices on the network
+    port: 3000,
+    https: true,
+    host: true,
     watch: {
       usePolling: true,
     },
