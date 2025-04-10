973aaf2d15bfc85733c6147952a0660653fd3500
Anastasiia Kuzmich <bbbbbgdn@icloud.com>
2025-04-10 20:42:00 +0100

    Trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import fs from 'fs';
 import path from 'path';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ADT-S/', 
   plugins: [vue(),
     basicSsl(),
   ],
