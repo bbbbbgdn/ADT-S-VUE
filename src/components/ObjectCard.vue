485b8ea26c1ca756e070be54ed696871844253df
Anastasiia Kuzmich <bbbbbgdn@icloud.com>
2025-04-27 18:07:55 +0100

    fixed ObjectCard

--- src/components/ObjectCard.vue
+++ src/components/ObjectCard.vue
@@ -58,7 +58,7 @@ export default {
   min-height: 200px;
   overflow: hidden;
   /* border-radius: 12px; <-- видалено */
-  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
+  /* box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1); */
 }
 
 .object-bg {
