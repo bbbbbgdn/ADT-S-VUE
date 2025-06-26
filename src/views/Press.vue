12d6346bedb6484e9081991c3b9763e9f54cb43a
Bogdan <4imzuk@gmail.com>
2025-06-26 20:41:57 +0200

    Press Background repeat

--- src/views/Press.vue
+++ src/views/Press.vue
@@ -140,7 +140,8 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background-size: cover;
+  background-size: contain;
+  background-repeat: no-repeat;
   background-position: center;
   pointer-events: none;
   z-index: -1;
